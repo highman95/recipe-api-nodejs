e66e6b75acd0e884310cd346a7cb77701612919e
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-10-27 21:08:42 +0100

    implemented the middlewares for the api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Recipe = require('./models/recipe');
 
 const app = express();
 
@@ -21,5 +22,41 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+app.post('/api/recipes', (req, res, next) => {
+    ({ title, ingredients, instructions, difficulty, time } = req.body);
+    const recipe = new Recipe({ title, ingredients, instructions, difficulty, time });
+
+    recipe.save()
+        .then(() => res.status(201).json({ message: 'Recipe created successfully...' }))
+        .catch(error => res.status(400).json({ error }))
+});
+
+app.put('/api/recipes/:id', (req, res, next) => {
+    ({ title, ingredients, instructions, difficulty, time } = req.body);
+    const recipe = new Recipe({ _id: req.params.id, title, ingredients, instructions, difficulty, time });
+
+    Recipe.updateOne({ _id: req.params.id }, recipe)
+        .then(() => res.status(201).json({ message: 'Recipe updated successfully...'}))
+        .catch(error => res.status(400).json({ error }))
+});
+
+app.delete('/api/recipes/:id', (req, res, next) => {
+    Recipe.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Recipe deleted successfully...' }))
+        .catch(error => res.status(400).json({ error }))
+});
+
+app.get('/api/recipes/:id', (req, res, next) => {
+    Recipe.findOne({ _id: req.params.id })
+        .then(recipe => res.status(200).json(recipe))
+        .catch(error => res.status(404).json({ error }))
+});
+
+app.get('/api/recipes', (req, res, next) => {
+    Recipe.find()
+        .then(recipes => res.status(200).json(recipes))
+        .catch(error => res.status(400).json({ error }))
+});
 
 module.exports = app;
